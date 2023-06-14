da139c6ed36604e07c543d011cfab9cb793c2d4c
Nicklas Utgaard <nutgaard@gmail.com>
2023-06-14 09:57:04 +0200

    fix(dep): handle missing memfs dependency
    memfs should only be using during test, and thus we need to handle that it is not present

--- src/utils/get-filesystem.ts
+++ src/utils/get-filesystem.ts
@@ -1,9 +1,18 @@
 import * as fs from 'fs';
-import memfs, { IFs } from 'memfs';
+import { IFs } from 'memfs';
+
+let memfs: IFs | null = null;
+try {
+    memfs = require('memfs').fs;
+} catch {}
 
 export function getMemFs(): IFs {
-    return memfs.fs;
+    if (memfs !== null) {
+        return memfs;
+    }
+    throw new Error('Illegally tried to access memfs, which should only be used for testing');
 }
+
 export function getFs(): IFs {
     return fs as unknown as IFs;
 }
